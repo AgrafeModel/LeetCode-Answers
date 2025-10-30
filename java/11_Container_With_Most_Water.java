009984e5ae28dbf7e2dbfdb0296f6828fc5e1e48
Epingle <73447649+AgrafeModel@users.noreply.github.com>
2025-10-30 14:56:15 +0100

    Create 11_Container_With_Most_Water.java

---
+++ java/11_Container_With_Most_Water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right = height.length -1;
+        int max = 0;
+        while(left<right){
+            int min = Math.min(height[left],height[right]) ;
+            max = Math.max(min * (right - left),max);
+            while(left<right && height[left]<=min){
+                left++;
+            }
+            while(left<right && height[right]<=min){
+                right--;
+            }
+        }
+
+        return max;
+    }
+}
